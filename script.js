15aa7801623893b380572f7ced3e179cd9f1aca0
Natali_L <nikabriz@gmail.com>
2021-08-14 11:56:31 +0500

    3 и 4 задания

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//задание 1 
 function convertTemp() {
     var c = document.getElementById('c');
     var f = document.getElementById('f');
@@ -10,3 +11,25 @@ function convertTemp() {
         f.value = '';
     }
 }
+
+// задание 3
+var admin;
+var name='Василий';
+
+admin = name;
+
+alert (admin);
+console.log (admin);
+
+
+
+//задание 4
+
+var a = 1000;
+var b='108';
+var rezult = a + b;
+
+alert(rezult);
+console.log(rezult);
+
+
