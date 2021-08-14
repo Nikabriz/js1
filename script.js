2eaf225f80da99f6b4c4ac89fe897c9fa2fd24b6
Natali_L <nikabriz@gmail.com>
2021-08-14 12:03:42 +0500

    2 вариант

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//задание 1 
+//задание 1  вариант 1
 function convertTemp() {
     var c = document.getElementById('c');
     var f = document.getElementById('f');
@@ -12,21 +12,31 @@ function convertTemp() {
     }
 }
 
+//задание 1 вариант 2
+
+var temp = prompt('Введите значение Цельсия ');
+var rezult = 9 / 5 * temp + 32;
+
+alert(rezult);
+console.log(rezult);
+
+
+
 // задание 3
 var admin;
-var name='Василий';
+var name = 'Василий';
 
 admin = name;
 
-alert (admin);
-console.log (admin);
+alert(admin);
+console.log(admin);
 
 
 
 //задание 4
 
 var a = 1000;
-var b='108';
+var b = '108';
 var rezult = a + b;
 
 alert(rezult);
